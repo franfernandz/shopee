02d68b1b2b3bb115e63a16d887ae3e3d7e916d69
Francisco Fernandez <franciscofernandez@MacBook-Air-de-Francisco.local>
2025-08-10 03:06:35 -0300

    Update de .gitgnore y db.js

--- shopee-backend/db.js
+++ shopee-backend/db.js
@@ -1,15 +1,13 @@
-// backend/db.js
+require('dotenv').config();
 
 const { Pool } = require('pg');
 
-// Es recomendable usar variables de entorno para datos sensibles
-// pero para este ejemplo, lo pondremos directamente.
 const pool = new Pool({
-  user: 'shopee_admin', // Tu nombre de usuario de PostgreSQL
-  host: '127.0.0.1',
-  database: 'perfumes', // El nombre de tu base de datos
-  password: 'sh0p332025',    // La contraseña de tu usuario
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 module.exports = pool;
